b55a8e7456c3ba8098b526522cc44d114faa8d08
NICKLIK <niko.negrete.v@gmail.com>
2025-10-21 17:39:52 -0500

    Cambios Time to Market II

---
+++ src/main/java/com/tuempresa/facturacionnegrete/modelo/Factura.java
@@ -0,0 +1,59 @@
+package com.tuempresa.facturacionnegrete.modelo;
+ 
+import java.time.*;
+import java.util.*;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+
+import org.hibernate.annotations.*;
+import org.openxava.annotations.*;
+import org.openxava.calculators.*;
+
+import com.tuempresa.facturacion.calculadores.*;
+
+import lombok.*;
+ 
+@Entity @Getter @Setter
+
+@View(members= 
+"anyo, numero, fecha;" + 
+"cliente;" + 
+"detalles;" +
+"observaciones"
+)
+
+public class Factura {
+
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @Hidden
+    @GenericGenerator(name="system-uuid", strategy="uuid")
+    @Column(length=32)
+    String oid;
+ 
+    @Column(length=4)
+    @DefaultValueCalculator(CurrentYearCalculator.class) 
+    int anyo;
+ 
+    @Column(length=6)
+    @DefaultValueCalculator(value=CalculadorSiguienteNumeroParaAnyo.class,
+    properties=@PropertyValue(name="anyo") 
+    )
+    int numero;
+ 
+    @Required
+    @DefaultValueCalculator(CurrentLocalDateCalculator.class)
+    LocalDate fecha;
+ 
+    @TextArea
+    String observaciones;
+     
+    @ElementCollection
+    @ListProperties("producto.numero, producto.descripcion, cantidad")
+    Collection<Detalle> detalles;
+    
+    @ManyToOne(fetch=FetchType.LAZY, optional=false)
+    @ReferenceView("Simple") 
+    Cliente cliente;
+}
